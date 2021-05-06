e60c88db9e5c00b58aceb262bfa846c7c4fe7573
maningning <maningning@kuaishou.com>
2021-05-06 20:55:52 +0800

    init

--- mytest/complex.h
+++ mytest/complex.h
@@ -8,18 +8,18 @@
 class complex
 {
  public:
-  complex(double r = 0, double i = 0)
+    complex(double r = 0, double i = 0)
     :re(r), im(i){}
     complex& operator +={const complex&};
-  double real () const {
-    return re;
-  }
-  double imag () const {
-    return im;
-  }
+    double real () const {
+      return re;
+    }
+    double imag () const {
+      return im;
+    }
  private:
-  double re, im;
-  friend complex&__doapl(complex*, const complex&);
+    double re, im;
+    friend complex&__doapl(complex*, const complex&);
 };
 
 #endif //__complex__
