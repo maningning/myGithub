a1c2679f16b1409caa4612302cd3ca7bcc55835a
maningning <maningning@kuaishou.com>
2021-05-06 20:52:18 +0800

    init

---
+++ mytest/complex.h
@@ -0,0 +1,25 @@
+//
+// Created by 马宁宁 on 2021/5/6.
+//
+
+#ifndef __COMPLEX__
+#define __COMPLEX___
+#include <cmath>
+class complex
+{
+ public:
+  complex(double r = 0, double i = 0)
+    :re(r), im(i){}
+    complex& operator +={const complex&};
+  double real () const {
+    return re;
+  }
+  double imag () const {
+    return im;
+  }
+ private:
+  double re, im;
+  friend complex&__doapl(complex*, const complex&);
+};
+
+#endif //MYGITHUB_MYTEST_COMPLEX_H_
