c3755a61e70093c84fdf7ce61faa60603cf585fa
maningning <maningning@kuaishou.com>
2021-05-07 15:14:48 +0800

    init

--- mytest/complex.h
+++ mytest/complex.h
@@ -36,23 +36,34 @@ complex::operator += (const complex& r) {
 }
 
 // Non-member function
-//inline complex
-//operator + (const complex& x, const complex& y){
-//  return complex(real(x) + real(y), imag(x) + imag(y));
-//}
-//
-//inline complex
-//operator + (const double x, const complex& y){
-//  return complex(x + real(y), imag(y));
-//}
-//
-//inline complex
-//operator + (const complex& x, const double y){
-//  return complex(real(x) + y, imag(x));
-//}
-//
-//ostream&
-//operator << (ostream& os, const complex& x){
-//  return os << '(' << real(x) << ',' <<imag(x) <<')';
-//}
+inline double
+real (const complex& x) {
+  return x.real ();
+}
+
+inline double
+imag (const complex& x) {
+  return x.imag ();
+}
+
+
+inline complex
+operator + (const complex& x, const complex& y){
+  return complex(real(x) + real(y), imag(x) + imag(y));
+}
+
+inline complex
+operator + (const double x, const complex& y){
+  return complex(x + real(y), imag(y));
+}
+
+inline complex
+operator + (const complex& x, const double y){
+  return complex(real(x) + y, imag(x));
+}
+
+std::ostream&
+operator << (std::ostream& os, const complex& x){
+  return os << '(' << real(x) << ',' <<imag(x) <<')';
+}
 #endif //__complex__
---
+++ mytest/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"complex.h"
+
+int main() {
+  complex c1(2, 1);
+  complex c2(4, 0);
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
